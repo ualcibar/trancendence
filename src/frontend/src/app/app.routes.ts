3a24a2f7b52af0251410a279a70db05b47c70d21
AllPlayed <unaillope@gmail.com>
2024-02-06 19:04:13 +0100

    Frontend funciona

--- src/frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { PongComponent } from './components/pong/pong.component';
-
-
-export const routes: Routes = [
-    {path: 'pong-game', component: PongComponent}
-];
