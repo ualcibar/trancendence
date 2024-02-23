142897cc041e930ac8b25c93a7579f757a9e7c93
Eneko Camara <ecamara@c3r6s1.42urduliz.com>
2024-02-23 16:09:51 +0100

    nothign

--- src/frontend/src/app/pages/home/home.component.ts
+++ src/frontend/src/app/pages/home/home.component.ts
@@ -13,17 +13,17 @@ export class HomeComponent implements OnInit{
     constructor(private route: ActivatedRoute) { }
     ngOnInit(): void {
         // Your initialization code here
-        console.log("unai fucking useless");
         this.route.queryParams.subscribe(params => {
             const code = params['code']; // Extract authorization code from query parameters
             // Now you can use the authorization code to obtain an access token
             if (code) {
                 // Handle the authorization code
                 console.log('Authorization code:', code);
+                
                 // Proceed with token exchange or authentication process
             } else {
                 // Handle the absence of authorization code
-                console.error('Authorization code not found');
+                console.log('Authorization code not found');
                 // Handle error or redirect to an error page
             }
         });
