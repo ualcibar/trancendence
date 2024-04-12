75dff66a3b462504a42c3f0e809ade0d381e53d2
AllPlayed <unaillope@gmail.com>
2024-04-12 18:25:49 +0200

    Paquetes actualizados y rediseño del perfil al 60%

--- src/turn/start.sh
+++ src/turn/start.sh
@@ -1,4 +1,5 @@
 openssl genrsa -out cert.key 2048
 openssl req -new -key cert.key -out cert.csr
 openssl x509 -req -days 3650 -in cert.csr -signkey cert.key -out cert.crt
-turnserver -c /etc/turnserver.conf
+# turnserver -c /etc/turnserver.conf
+turnserver -L localhost -o -a -f -r turnRealm
