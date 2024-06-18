4a6e326d67821eaba8e196d5b7b075cb6f5f555b
f4su <joandrade.gai@gmail.com>
2024-06-18 13:06:54 +0200

    hola branch

--- src/frontend/angular/src/main.ts
+++ src/frontend/angular/src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 // this is going to be replaced by start.sh
-var ip = "localhost";
+var ip = "";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(error => console.error(error));
