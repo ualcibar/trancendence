cf9cf473b1879fa3984209ca6f213fc26a977f6e
Llopeando <unaillope@gmail.com>
2024-06-18 14:36:43 +0200

    Pequeños cambios

--- src/ip_to_env.sh
+++ src/ip_to_env.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo "IP=$(ip a | grep -A2 'enp4s0f0:' | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)" >> .env
+IP=$(ip a | grep -A2 'enp4s0f0:' | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+
+#If the first one does not work:
+if [ -z "$IP" ]; then
+  echo "Auxiliary IP setted"
+  IP=$(ip a | grep -A2 '1:' | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+fi
+
+echo "IP=$IP" >> .env
