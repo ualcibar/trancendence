711ad1f1c52741c781337ab94a5c9020cea44b36
f4su <joandrade.gai@gmail.com>
2024-06-19 19:40:17 +0200

    tournament ui

--- src/frontend/angular/src/main.ts
+++ src/frontend/angular/src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 // this is going to be replaced by start.sh
-export var ip = "127.0.0.1";
+export var ip = "10.13.8.3";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(error => console.error(error));
